981c33553960c427f228acecb3c9e694c86e939d
FlowerSamda <flowersamda@gmail.com>
2021-06-02 14:52:24 +0000

    C to F 변환 과제완료!

--- learning/functions_and_loop/loops/src/main.rs
+++ learning/functions_and_loop/loops/src/main.rs
@@ -17,10 +17,15 @@ fn main() {
         println!("number: {}", number);
         number -= 1;
     }
-    println!("발사!")
+    println!("발사!");
+
+    let faren = c_to_f_inverter(36.0);
+    println!("celcius 36.0: farenheit {}.", faren);
 }
 
 
 //과제 - 섭씨 -> 화씨 온도 변환기
 
-fn c_to_f_inverter
+fn c_to_f_inverter(celcius: f64) -> f64 {
+    celcius * 1.8 + 32.0
+}
---
+++ learning/functions_and_loop/loops/target/debug/deps/loops-6106f59a87f9b0c5.d
@@ -0,0 +1,5 @@
+/workspace/Rust/learning/functions_and_loop/loops/target/debug/deps/loops-6106f59a87f9b0c5.rmeta: src/main.rs
+
+/workspace/Rust/learning/functions_and_loop/loops/target/debug/deps/loops-6106f59a87f9b0c5.d: src/main.rs
+
+src/main.rs:
