f5ebfc90795fea3a87f202bc7026287561704f93
FlowerSamda <flowersamda@gmail.com>
2021-06-10 11:58:47 +0000

    lifetime

--- learning/lifetime/src/main.rs
+++ learning/lifetime/src/main.rs
@@ -88,6 +88,124 @@ fn main() {
 	
 	
 	
+	// 구조체 정의 상에서의 라이프타임 명시!
+	
+	struct ImportntExcerpt<'a> {
+		part: &'a str,
+	}
+	
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+	
+	// 라이프타임 명시 없이 컴파일되는 함수 (원래는 라이프타임 명시가 필요했으나, 예측가능하기에 생략하게됨)
+	// -> 라이프타임 생략 규칙
+	fn first_word(s: &str) -> &str {
+		let bytes = s.as_bytes();
+
+		for (i, &item) in bytes.iter().enumerate() {
+			if item == b' ' {
+				return &s[0..i];
+			}
+		}
+
+		&s[..]
+	}
+	
+	// 라이프타임 규칙
+	// 1. 함수나 메소드의 파라미터에 대한 라이프타임 = 입력 라이프타임 (input lifetime)
+	/*
+	    -> 참조자인 각각의 파라미터는 고유한 라이프타임 파라미터를 가짐.
+		하나의 파라미터를 가지는 함수는 하나의 라이프타임 파라미터 fn foo<'a>(x: &'a i32)
+		두개의 파라미터를 갖는 함수는 두개의 라이프타임 파라미터를 따로 가짐 fn foo<'a, 'b> (x: &'a i32, y: &'b i32)
+	*/
+	
+	// 2. 반환 값에 대한 라이프타임                = 출력 라이프타임 (output lifetime)
+	/*
+	    -> 만일 정확히 딱 하나의 라이프타임 파라미터만 있다면, 그 라이프타임이 모든 출력 라이프타임 파라미터들에 대입된다.
+	    fn foo<'a> (x:&'a i32) -> &'a i32
+		
+		-> 만일 여러개의 입력 파라미터가 있는데, 메소드라서 그중 하나가 &self 혹은 &mut self일 시,
+		self의 라이프타임이 모든 출력 라이프타임 파라미터에 대입됨
+	*/
+	
+	
+	//	3. 예시 함수들에 대한 적용
+	/*
+		* 하나의 참조 파라미터 입력이 있는 함수
+		
+		1)
+		fn first_word(s: &str) -> &str  입력 라이프타임 1번 규칙: 참조자 파라미터는 각각 고유한 라이프타임 파라미터를 가진다
+		->fn first_word<'a>(s:'a &str) -> &str
+		
+		2) fn first_word<'a>(s:'a &str) -> &str  
+		출력 라이프타임 1번 규칙: 딱 하나의 라이프타임 파라미터가 있기에, 이 라이프타임이 모든 출력 라이프타임 파라미터들에 대입.
+		-> fn first_word<'a>(s:'a &str) -> &'a str
+		
+		* 두개 이상의 참조 입력 파라미터가 있는 함수
+		
+		1)
+		fn longest(x: &str, y: &str) -> &str {}  입력: x ,y 둘 다 &str -> 1번 규칙으로 두개의 고유한 라이프타임 가짐
+		-> fn longest<'a, 'b>(x:'a &str, y:'b &str) -> &str {}
+		
+		2) fn longest<'a, 'b>(x:'a &str, y:'b &str) -> &str {}  출력: 2, 3번 둘 다 해당 X(1개도 아니고, 메서드도 아님)
+		-> 반환 참조자의 라이프타임이 무엇인지 알아낼 수 없기에, 오류를 내보냄 -> 직접 지정 필요!
+	*/
+	
+	
+	
+	// 메소드 정의 내에서의 라이프타임 명시 (라이프타임을 가진 구조체 메소드)
+	// 라이프타임 파라미터가 선언되고 사용되는 곳: 라이프타임 파라미터가 구조체의 필드들 혹은 메소드 인자, 반환값과 연관이 있는지 없는지에 달림!
+	
+	// 구조체 필드를 위한 라이프타임 이름은 언제나 impl 키워드 뒤에 선언되어야함!
+	// -> 그러고 나서, 구조체 이름 뒤에 사용되야하는데, 이 라이프타임들은 구조체 타입의 일부이기 떄문!
+	
+	
+	// impl뒤와 타입이름 뒤에서 이를 사용하는건 메소드 정의에서 필요한 것
+	
+	// 그냥 구조체
+	struct ImportantExcerpt<'a> {
+    	part: &'a str,
+	}
+	// 그냥 i32를 반환하므로, 참조자 선언(<'a>)필요 없음
+	impl<'a> ImportantExcerpt<'a> {
+		// 첫번째 생략규칙(하나의 파라미터를 가지는 함수는 하나의 라이프타임 파라미터만 필요)때문에, self로의 참조자 라이프타임 명시 필요 X
+		fn level(&self) -> i32 {
+			3
+		}
+	}
+	
+	impl<'a> ImportantExcerpt<'a> {
+		//3번째 라이프타임 규칙에 의해 필요 없는 것 = 메소드라서, self(객체)의 라이프타임이 모두 적용됨
+    	fn announce_and_return_part(&self, announcement: &str) -> &str {
+        	println!("Attention please: {}", announcement);
+        	self.part
+    	}
+	}
+
+	// 정적 라이프타임(Static lifetime)
+	
+	// 'static 라이프타임은 프로그램의 전체 생애주기를 가리킴.
+	let s: &'static str = "I have a static lifetime.";
+	
+	
+	//제네릭 타입 파라미터, 트레잇 바운드, 라이프타임을 함께 써보기
+	
+	use std::fmt::Display;
+	//                             라이프타임 명시, 제네릭 T 선언          T 사용
+	fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display  // 트레잇 바운드 (T타입은 Display를 구현하고 있어야 함)
+	{
+		println!("Announcement! {}", ann);  // Display 구현이 필요한 이유
+		if x.len() > y.len() {
+			x
+		} else {
+			y
+		}
+	}
+	
 	
 }
 
