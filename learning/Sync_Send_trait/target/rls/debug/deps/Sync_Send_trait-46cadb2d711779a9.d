722a3bf80a11f09f6109c002ee6aa41299d440e3
FlowerSamda <flowersamda@gmail.com>
2021-07-01 14:22:16 +0000

    동시성&병렬성 완

---
+++ learning/Sync_Send_trait/target/rls/debug/deps/Sync_Send_trait-46cadb2d711779a9.d
@@ -0,0 +1,5 @@
+/home/flowersamda/Rust/learning/Sync_Send_trait/target/rls/debug/deps/Sync_Send_trait-46cadb2d711779a9.rmeta: src/main.rs
+
+/home/flowersamda/Rust/learning/Sync_Send_trait/target/rls/debug/deps/Sync_Send_trait-46cadb2d711779a9.d: src/main.rs
+
+src/main.rs:
---
+++ learning/Sync_Send_trait/target/rls/debug/deps/Sync_Send_trait-fb7a7e8512e9a15e.d
@@ -0,0 +1,5 @@
+/home/flowersamda/Rust/learning/Sync_Send_trait/target/rls/debug/deps/Sync_Send_trait-fb7a7e8512e9a15e.rmeta: src/main.rs
+
+/home/flowersamda/Rust/learning/Sync_Send_trait/target/rls/debug/deps/Sync_Send_trait-fb7a7e8512e9a15e.d: src/main.rs
+
+src/main.rs:
